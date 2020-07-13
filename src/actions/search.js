5f71b06faf20e7a22466b45bd70cdf202a2b3c30
Gabriel Song <gabsong4@gmail.com>
2020-07-12 18:20:40 -0700

    Define action creators
    changeVideo and changeVideoList return plain objects (sync).
    
    handleVideoSearch is async, so it returns a function that dispatches other actions within a callback
    when the async code is finished executing.

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,13 +3,14 @@ import changeVideoList from './videoList.js'
 import changeVideo from './currentVideo.js'
 import YOUTUBE_API_KEY from '../config/youtube.js'
 
+// https://stackoverflow.com/questions/46765896/react-redux-actions-must-be-plain-objects-use-custom-middleware-for-async-acti
 var handleVideoSearch = (q) => {
-  // TODO:  Write an asynchronous action to handle a video search!
-  // "before all" hook for "should return a function"
-  // Error: Actions must be plain objects. Use custom middleware for async actions.
-  //   at dispatch (webpack-internal:///./node_modules/redux/es/redux.js:208:13)
-  //   at Object.eval [as dispatch] (webpack-internal:///./node_modules/redux-thunk/es/index.js:12:16)
-  //   at Context.eval (webpack-internal:///./spec/reduxSpec/actions.jsx:57:65)
+  return (dispatch) => {
+    searchYouTube({ key: YOUTUBE_API_KEY, q }, (videos) => {
+      dispatch(changeVideoList(videos))
+      dispatch(changeVideo(videos[0]))
+    })
+  }
 }
 
 export default handleVideoSearch
