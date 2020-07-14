1ee54f90f077a4cab49d1c9feca6b48667fab279
Gabriel Song <gabsong4@gmail.com>
2020-07-14 00:23:52 -0700

    Implement container components for redux

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,21 @@
-import { connect } from 'react-redux';
-import Search from './../components/Search.js';
-import handleSearchChange from '../actions/search.js';
+import { connect } from 'react-redux'
+import Search from './../components/Search.js'
+import handleVideoSearch from '../actions/search.js'
 
-var SearchContainer = () => {};
-
-//TODO: define a SearchContainer component which will hook up your action
+// TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
-//HINT: use react-redux 'connect' method to generate a container component from
-//state and dispatch mappings.
+// HINT: use react-redux 'connect' method to generate a container component from
+// state and dispatch mappings.
+const mapStateToProps = (state) => ({
+  value: state.value
+})
 
-export default SearchContainer;
+const mapDispatchToProps = (dispatch) => ({
+  handleSearchInputChange: (q) => {
+    dispatch(handleVideoSearch(q))
+  }
+})
+
+const SearchContainer = connect(mapStateToProps, mapDispatchToProps)(Search)
+
+export default SearchContainer
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -1,10 +1,19 @@
-import { connect } from 'react-redux';
-import VideoList from './../components/VideoList.js';
-import changeVideo from './../actions/currentVideo.js';
+import { connect } from 'react-redux'
+import VideoList from './../components/VideoList.js'
+import changeVideo from './../actions/currentVideo.js'
 
-var VideoListContainer = () => {};
-
-//TODO: define a VideoListContainer component which will hook up your action
+// TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
+const mapStateToProps = (state) => ({
+  videos: state.videoList
+})
 
-export default VideoListContainer;
+const mapDispatchToProps = (dispatch) => ({
+  handleVideoListEntryTitleClick: (video) => {
+    dispatch(changeVideo(video))
+  }
+})
+
+const VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList)
+
+export default VideoListContainer
